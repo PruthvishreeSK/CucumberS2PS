06501c07a38875c76327002760ec0815beda3c0f
PruthvishreeSK <145194080+PruthvishreeSK@users.noreply.github.com>
2023-12-05 00:01:37 +0530

    Add files via upload

---
+++ src/main/java/CucumberS2PS/CucumberS2PS/App.java
@@ -0,0 +1,13 @@
+package CucumberS2PS.CucumberS2PS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
